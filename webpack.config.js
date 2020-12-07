a8c20a50c49d0c20431f510bffe865e1460c49e4
TaehwanGo <gth1123@naver.com>
2020-12-07 21:31:26 +0900

    8. plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path'); // nodejs에서 파일의 경로를 쉽게 핸들링 할 수 있게 해주는 부품
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // 로더는 로더의 이름을 rules에 적었지만
+// plugin은 직접가져와서 실행시켜야함
 
 module.exports = {
   mode:"development",
@@ -21,6 +23,18 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template:'./source/index.html',
+      filename:'./index.html',
+      chunks:['index'], // 원하는 js파일만 추가 하고 싶으므로 entry의 이름과 같은 것을 넣어줌 ''빼먹으면 실행안됨
+    }),
+    new HtmlWebpackPlugin({
+      template:'./source/about.html',
+      filename:'./about.html',
+      chunks:['about'], // 원하는 js파일만 추가 하고 싶으므로 entry의 이름과 같은 것을 넣어줌 ''빼먹으면 실행안됨
+    })
+  ]
 };
 // npx webpack --entry ./source/index.js --output ./public/index_bundle.js 와 똑같은 코드
 
